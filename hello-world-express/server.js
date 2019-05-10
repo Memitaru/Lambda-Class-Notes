0c4f103666db37d578e40b32d17082a2bd87400e
Memitaru <amandatwah@gmail.com>
2019-05-09 17:16:03 -0700

    Practice learning express and node

---
+++ hello-world-express/server.js
@@ -0,0 +1,21 @@
+// const http = require('http');
+
+const express = require('express');
+
+const port = 5000;
+
+const server = express();
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World, from NodeJS');
+// });
+
+server.get('/', (req, res) => {
+    res.send('Hello World from Express!');
+})
+
+server.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+});
